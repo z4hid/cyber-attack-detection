336ab8ee753f17e44c4ecb596f05b39a5c0c8d0e
zahidhasanshuvo <zahidhasan9297@gmail.com>
2023-08-17 03:00:10 +0600

    1st UI Update

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
 import os
 import time
 import numpy as np
-import streamlit as st
 from PIL import Image
+import streamlit as st
 from tensorflow.keras.models import load_model
+
+# Constants
+MODEL_PATH = "models/model.h5"
+CLASS_NAMES = [
+    'AgentTesla', 'Amadey', 'AsyncRAT', 'Emotet', 'FormBook', 'GuLoader',
+    'Loki', 'Mirai', 'NetSupportRAT', 'NjRat', 'Non-Malicious', 'QBot',
+    'RedLineStealer', 'Remcos', 'Vidar'
+]
+
+# Functions
+def load_ml_model(path):
+    """Load the trained machine learning model from the specified path."""
+    return load_model(path)
+
+def classify_uploaded_image(img):
+    """Classify the uploaded image using the trained model."""
+    img = Image.open(img).convert('RGB').resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions[0])
+    predicted_class_name = CLASS_NAMES[predicted_class]
+    predicted_percentage = predictions[0][predicted_class] * 100
+    return predicted_class_name, predicted_percentage
 
 # Configuration settings
 st.set_page_config(
@@ -13,7 +37,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-# Apply custom styles for a polished appearance
+# Custom styling for the Streamlit app
 st.markdown(
     """
     <style>
@@ -33,37 +57,13 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# Set environment variable to use CPU only
+# Force TensorFlow to use CPU
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-# Constants
-MODEL_PATH = "models/model.h5"
-CLASS_NAMES = [
-    'AgentTesla', 'Amadey', 'AsyncRAT', 'Emotet', 'FormBook', 'GuLoader',
-    'Loki', 'Mirai', 'NetSupportRAT', 'NjRat', 'Non-Malicious', 'QBot',
-    'RedLineStealer', 'Remcos', 'Vidar'
-]
-
-
-def load_ml_model(path):
-    """Load machine learning model from a given path."""
-    return load_model(path)
-
-
-def classify_uploaded_image(img):
-    """Classify the uploaded image using a pre-trained model."""
-    img = Image.open(img).convert('RGB').resize((224, 224))
-    img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    predictions = model.predict(img_array)
-    predicted_class = np.argmax(predictions[0])
-    predicted_class_name = CLASS_NAMES[predicted_class]
-    predicted_percentage = predictions[0][predicted_class] * 100
-    return predicted_class_name, predicted_percentage
-
-
+# Load model
 model = load_ml_model(MODEL_PATH)
 
+# Streamlit UI
 st.title("Cyber Attack Detection using A.I. 🛡️")
 st.write("Utilize our state-of-the-art AI model to identify potential malware from image representations.")
 uploaded_file = st.file_uploader(label="Upload File")
@@ -84,8 +84,6 @@ if uploaded_file:
         if st.button("Scan Now", help="Click to start malware detection"):
             with st.spinner("Scanning..."):
                 progress_bar = st.progress(0)
-                predicted_class_name = None
-
                 for percent_complete in range(0, 101, 10):
                     time.sleep(0.1)
                     progress_bar.progress(percent_complete)
